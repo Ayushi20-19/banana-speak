b51810ae55717154cd68a47d57ba7b90f92846d5
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2020-12-15 20:51:54 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // // alert("cbhbsdcjj")
 // //api talking with browser---user is talking to the 
-// //if we want to program anythig we want api,,45time stamp
+// //if we want to program anything we want api,(45 time stamp)
 // var btnTranslate = document.querySelector("#btn-translate");
 // // console.log(btnTranslate);
 // // event addEventListener, what event to listened "click event" call back if this is clicked do this
 // btnTranslate.addEventListener("click", function clickEventHandler(){
-//     console.log("clicked")
+//     console.log("clicked")**/*+ 
 // })
 
 
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
 
 function clickHandler(){
-    console.log("clicked")
-    console.log("input" , txtInput.value);
+    outputDiv.innerText = "ajjsjsjsjs " + txtInput.value;
+    // console.log("clicked")
+    // console.log("input" , txtInput.value);
 };
 
 btnTranslate.addEventListener("click", clickHandler)
